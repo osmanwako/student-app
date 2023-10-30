c0da9cbf8098bbe88121269cebe64048825ee204
OsmanW <osmanwako411@gmail.com>
2023-10-30 11:46:37 +0300

    Add Hook

--- src/component/home.js
+++ src/component/home.js
@@ -2,9 +2,9 @@ import React from "react";
 
 class Home extends React.Component {
     render() {
-        return <h3>
-            This is home page
-        </h3>;
+        return <div className="home">
+            <h1>This is home</h1>
+        </div>;
     }
 }
 
